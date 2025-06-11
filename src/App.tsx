a2f19bef1d6b2dab81f2a80041de988e60fee7e6
andrei <35298550+d4rckh@users.noreply.github.com>
2025-06-11 15:55:06 +0300

    design mai aratos

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col">
-        <p className="text-4xl">Provocarea Alimentară Zilnică</p>
-      
-        <Card className="mt-10">
-          <CardTitle className="text-3xl">Provocarea de {offset == 0 && "azi"}{offset == -1 && "ieri"}{offset < -1 && "acum " + (-offset) + " zile"} {offset == 0 ? "este" : "era"}: </CardTitle>
+      <div className="flex flex-col px-2">
+        <p className="text-4xl text-shadow-2xs font-black text-center mt-10">Provocarea Alimentară Zilnică</p>
+
+        <Card className="mt-10 text-center">
           <CardContent>
+            <CardTitle className="text-3xl mb-4">Provocarea de {offset == 0 && "azi"}{offset == -1 && "ieri"}{offset < -1 && "acum " + (-offset) + " zile"} {offset == 0 ? "este" : "era"}: </CardTitle>
             {todayChallenge(offset)}
           </CardContent>
           <CardFooter className="flex sm:flex-row flex-col gap-2 mx-auto">
@@ -26,6 +26,11 @@ function App() {
             }
           </CardFooter>
         </Card>
+
+        <div className="text-center mt-10">
+          Site creat de Sonea Andrei pentru concursul Școala Creativă cu tema ALIMENTAȚIA PE BAZE ȘTIINȚIFICE<br/>
+          Colegiul Național "Vasile Alecsandri" Iași, profesor coordonator Rusu Beatris
+        </div>
       </div>
     </>
   )
